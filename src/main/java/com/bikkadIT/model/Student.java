f0259d7a330c604967f09401267bb05ee5d8c295
NikitaDevkar <nikitadevkar387@gmail.com>
2022-09-12 23:53:52 +0530

    SMS-01:Entity class Created

--- src/main/java/com/bikkadIT/model/Student.java
+++ src/main/java/com/bikkadIT/model/Student.java
@@ -1,5 +1,42 @@
 package com.bikkadIT.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+	@Id
+private int Sid;
+private String Sname;
+private String Semail;
+private int Sage;
+public int getSid() {
+	return Sid;
+}
+public void setSid(int sid) {
+	Sid = sid;
+}
+public String getSname() {
+	return Sname;
+}
+public void setSname(String sname) {
+	Sname = sname;
+}
+public String getSemail() {
+	return Semail;
+}
+public void setSemail(String semail) {
+	Semail = semail;
+}
+public int getSage() {
+	return Sage;
+}
+public void setSage(int sage) {
+	Sage = sage;
+}
+@Override
+public String toString() {
+	return "Student [Sid=" + Sid + ", Sname=" + Sname + ", Semail=" + Semail + ", Sage=" + Sage + "]";
+}
 
 }
