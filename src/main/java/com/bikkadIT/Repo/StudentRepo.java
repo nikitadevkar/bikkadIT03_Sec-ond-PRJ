c3f6564d1b16f4565cd0ab918a01674bbc53609f
NikitaDevkar <nikitadevkar387@gmail.com>
2022-09-13 13:46:47 +0530

    SMS-04 :StudentController class created

--- src/main/java/com/bikkadIT/Repo/StudentRepo.java
+++
@@ -1,9 +0,0 @@
- package com.bikkadIT.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bikkadIT.model.Student;
-
-public interface StudentRepo extends JpaRepository<Student, Integer> {
-
-}
